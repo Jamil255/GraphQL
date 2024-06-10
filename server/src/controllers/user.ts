858ce13cd211da7c00b89cd69ca13a747d8b4fdd
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2024-06-10 22:17:50 +0500

    Update user.ts

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -3,6 +3,7 @@ import userModel from '../models/userSchema.js'
 export const getAllUsers = async () => {
   try {
     const result = await userModel.find()
+    consolle.log(result)
     return result
   } catch (error) {
     console.log(error)
